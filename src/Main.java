bd77657c653dd0987458863278b443840ead395a
Obed Anku <ankuobed@gmail.com>
2024-06-17 17:03:03 +0000

    new

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,10 @@ public class Main {
         Supplier supplier1 = new Supplier(1, "Supplier A", "123 Main St", "555-1234");
         Supplier supplier2 = new Supplier(2, "Supplier B", "456 Oak Rd", "555-5678");
 
-            System.out.println(supplier1.getSupplierId());
+        Drug drug1 = new Drug(22, "Weed", "A dangerous drug", 20.00, 2, 10, 50);
+
+        System.out.println(drug1.getDrugDescription());
+
 
         }
 };
