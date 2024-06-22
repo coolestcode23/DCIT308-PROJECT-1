f7c94424c1c146287d4b8fbdd0a8b883963dc5f8
Benaiah <bendametey@gmail.com>
2024-06-22 16:23:03 +0000

    database implementation

---
+++ src/main/java/MyJDBC.java
@@ -0,0 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MyJDBC {
+    private static String url = "jdbc:mysql://127.0.0.1:3306/pharmacy_database";
+    private static String user = "root";
+    private static String password = "password";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM DRUG");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("drugname"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.school.pharmacyui {
     requires javafx.controls;
     requires javafx.fxml;
     requires jdk.jshell;
+    requires java.sql;
 
 
     opens org.school.pharmacyui to javafx.fxml;
